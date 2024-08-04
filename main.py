c2c75ec1f42c20aec772bdacc05959996578b075
Nairu Guan <guan.nairu@gmail.com>
2024-08-03 23:06:03 -0400

    Added offset and limit for getting list of video subtitles

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, BackgroundTasks
+from fastapi import Depends, FastAPI, BackgroundTasks, HTTPException, Query
 from sqlalchemy import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -36,18 +36,29 @@ async def get_video_sub(video_id: str, background_tasks: BackgroundTasks, user_p
 
         return {"result": sub}
 
-# Get all subtitles by channel ID or playlist ID, lang_code (default 'zh')
+# Get all subtitles by channel name or playlist ID, lang_code (default 'zh')
 @app.get("/subtitles")
-async def all_subs(channel_id: str | None = None, playlist_id: str | None = None, lang_code: str | None = 'zh', session: AsyncSession = Depends(get_db)):
-    if channel_id:
-        selete_channel = select(Subtitle).where(Subtitle.channel_id == channel_id)
-        channel_videos = await session.exec(selete_channel)
-        result = channel_videos.scalars().all()
-    if playlist_id:
-        select_playlist = select(Subtitle).where(Subtitle.playlist_id == playlist_id)
-        playlist_videos = await session.exec(select_playlist)
-        result = playlist_videos.scalars().all()
-    return {"results": result}
+async def all_subs(channel_name: str | None = None, playlist_id: str | None = None, lang_code: str | None = 'zh', offset: int = 0, limit: int = Query(default=10, le=100), session: AsyncSession = Depends(get_db)):
+    try:
+        if channel_name:
+            channel_statement = select(Subtitle).where(Subtitle.channel_name == channel_name).offset(offset).limit(limit)
+            channel_videos = await session.exec(channel_statement)
+            result = channel_videos.scalars().all()
+        elif playlist_id:
+            playlist_statement = select(Subtitle).where(Subtitle.playlist_id == playlist_id).offset(offset).limit(limit)
+            playlist_videos = await session.exec(playlist_statement)
+            result = playlist_videos.scalars().all()
+        else:
+            statement = select(Subtitle).offset(offset).limit(limit)
+            all_videos = await session.exec(statement)
+            result = all_videos.scalars().all()
+        
+        if not result:
+            raise HTTPException(status_code=404, detail="Videos not found")
+        
+        return {"results": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Server error getting videos from database")
 
 
 # Get channel names
@@ -56,4 +67,7 @@ async def all_channels(session: AsyncSession = Depends(get_db)):
     statement = select(Subtitle.channel_name).distinct()
     channels = await session.exec(statement)
     result = channels.scalars().all()
+    if not result:
+        raise HTTPException(status_code=404, detail="Channel list not found")
+    
     return {"results": result}
